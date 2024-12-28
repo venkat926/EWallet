a57fb40297ff6c9355f18917fd0725fa5ab6466e
Raghu <k.raghu9926@gmail.com>
2024-12-28 14:49:50 +0530

    Initial Commit

---
+++ src/main/java/org/kvn/EWallet/EWalletApplication.java
@@ -0,0 +1,13 @@
+package org.kvn.EWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kvn/EWallet/EWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kvn.EWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
